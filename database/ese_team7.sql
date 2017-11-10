a9fcf2023b02b45e9fd047bb80ce51664d6d2ec7
Yves Chapuis <yveschapuis@me.com>
2017-11-10 08:21:07 +0100

    added a new database script

--- database/ese_team7.sql
+++ database/ese_team7.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 02, 2017 at 11:20 AM
+-- Generation Time: Nov 10, 2017 at 08:20 AM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.9
 
@@ -50,7 +50,9 @@ INSERT INTO `address` (`id`, `name`, `plz`, `street`, `town`) VALUES
 (7, 'Happy Animals Farm', 1212, 'Heavenstreet 12', 'Heaven'),
 (8, 'Evil Slaughterhouse', 6666, 'Devilroad 666', 'Hell'),
 (9, 'Happy Animals Farm', 1212, 'Heavenstreet 12', 'Heaven'),
-(10, 'Evil Slaughterhouse', 6666, 'Devilroad 666', 'Hell');
+(10, 'Evil Slaughterhouse', 6666, 'Devilroad 666', 'Hell'),
+(11, 'Farmer', 1234, 'First Street', 'Special Town'),
+(12, 'Manager', 5678, 'C Road', 'One City');
 
 -- --------------------------------------------------------
 
@@ -71,17 +73,19 @@ CREATE TABLE `ani_order` (
   `until` date DEFAULT NULL,
   `vehicle` varchar(255) DEFAULT NULL,
   `from_addr_id` int(11) DEFAULT NULL,
-  `to_addr_id` int(11) DEFAULT NULL
+  `to_addr_id` int(11) DEFAULT NULL,
+  `driver_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `ani_order`
 --
 
-INSERT INTO `ani_order` (`id`, `driver`, `number_of_animals`, `order_status`, `start_time`, `status_message`, `time_estimation`, `timeframe`, `type_of_animal`, `until`, `vehicle`, `from_addr_id`, `to_addr_id`) VALUES
-(1, 'Mathias Fuchs', 666, 'undelivered', '6666-06-06 06:00:00', 'Exploded on the way, sorry..', 6.6, '06:00 - 18:00', 'Sheep', '6666-06-06', 'Big Transporter', 9, 10),
-(2, 'John Snow', 10, 'undelivered', '2017-11-12 00:00:00', '', 7, '10:00-12:00', 'Horse', '2017-11-12', 'Medium Transporter', 3, 4),
-(3, 'Harry Potter', 2, 'undelivered', '2017-12-22 10:00:00', '', 4, '14:00-18:00', 'Owls', '2017-12-22', 'Tiny Transporter', 5, 6);
+INSERT INTO `ani_order` (`id`, `driver`, `number_of_animals`, `order_status`, `start_time`, `status_message`, `time_estimation`, `timeframe`, `type_of_animal`, `until`, `vehicle`, `from_addr_id`, `to_addr_id`, `driver_id`) VALUES
+(1, 'Mathias Fuchs', 666, 'undelivered', '6666-06-06 06:00:00', 'Exploded on the way, sorry..', 6.6, '06:00 - 18:00', 'Sheep', '6666-06-06', 'Big Transporter', 9, 10, 1),
+(2, 'John Snow', 10, 'undelivered', '2017-11-12 00:00:00', '', 7, '10:00-12:00', 'Horse', '2017-11-12', 'Medium Transporter', 3, 4, 2),
+(3, 'Harry Potter', 2, 'undelivered', '2017-12-22 10:00:00', '', 4, '14:00-18:00', 'Owls', '2017-12-22', 'Tiny Transporter', 5, 6, 3),
+(4, 'Mathias Fuchs', 4, 'undelivered', '2017-11-09 08:00:00', '', 2, '10:00 - 12:00', 'Cows', '2017-11-09', 'Big Transporter', 11, 12, 1);
 
 -- --------------------------------------------------------
 
@@ -204,19 +208,19 @@ ALTER TABLE `vehicle`
 -- AUTO_INCREMENT for table `address`
 --
 ALTER TABLE `address`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
 
 --
 -- AUTO_INCREMENT for table `ani_order`
 --
 ALTER TABLE `ani_order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `new_order`
 --
 ALTER TABLE `new_order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `user`

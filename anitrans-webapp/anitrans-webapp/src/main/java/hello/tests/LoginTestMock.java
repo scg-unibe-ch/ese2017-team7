8238c2a02af6c26e26ff9c130ac20b640d2dd742
Yves Chapuis <yveschapuis@me.com>
2017-12-06 16:01:09 +0100

    testing page content now works

--- anitrans-webapp/anitrans-webapp/src/main/java/hello/tests/LoginTestMock.java
+++ anitrans-webapp/anitrans-webapp/src/main/java/hello/tests/LoginTestMock.java
@@ -21,11 +21,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.security.test.context.support.WithMockUser;
+
 import static org.hamcrest.Matchers.containsString;
 import org.springframework.test.web.servlet.*;
 
@@ -34,9 +38,10 @@ import org.springframework.test.web.servlet.*;
 /*
  * Tests if the home page loads correctly.
  */
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@ContextConfiguration
 public class LoginTestMock {	
 	@Autowired
 	private MockMvc mockMvc;
@@ -54,6 +59,14 @@ public class LoginTestMock {
 		this.mockMvc.perform(post("/login")).andDo(print())
 	               .andExpect(status().isForbidden());
 	}
+	
+	//Tests that a logged in user will se the logout page
+		@Test
+		@WithMockUser(roles={"USER"})
+		public void logoutTitle() throws Exception {
+			this.mockMvc.perform(get("/login")).andDo(print()).andExpect(status().isOk())
+			.andExpect(content().string(containsString("Logout")));
+		}
 
 }
 
